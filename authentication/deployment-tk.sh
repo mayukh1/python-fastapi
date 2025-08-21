ad71c5c19d0abfa9d6115a07f912f596d400412c
mayukh1 <mallick.mayukh@gmail.com>
2025-08-21 20:09:26 +0530

    Create deployment-tk.sh

---
+++ authentication/deployment-tk.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+# ==== CONFIG ====
+export NS="fastapi-tk"
+export username="xxxxx"
+export password="xxxxxxxxxx"
+export APP="FastApi-Auth"
+export REGISTRY="image-registry.openshift-image-registry.svc:5000/python-fastapi"
+export PORT="8000"
+
+
+cat <<EOF | oc apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $APP
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: $APP
+  template:
+    metadata:
+      labels:
+        app: $APP
+    spec:
+      containers:
+        - name: authentication
+          image: $REGISTRY/authentication:latest
+          ports:
+            - containerPort: $PORT
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: $APP
+spec:
+  selector:
+    app: $APP
+  ports:
+    - port: $PORT
+      targetPort: $PORT
+---
+apiVersion: route.openshift.io/v1
+kind: Route
+metadata:
+  name: $APP
+spec:
+  to:
+    kind: Service
+    name: $APP
+  port:
+    targetPort: $PORT
+  tls:
+    termination: edge
+EOF
